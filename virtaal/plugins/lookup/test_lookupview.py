44cf88da8b4604292e2f2a4e1691783ed835952e
Dwayne Bailey <dwayne@translate.org.za>
2026-09-15 15:25:07 +0100

    Show a top-level menu item's submenu recursively, not just the item itself
    show() alone left Thesaurus's own submenu (the actual synonym
    entries) built but never shown - an empty-looking flyout, even though
    the data was really there.

--- virtaal/plugins/lookup/test_lookupview.py
+++ virtaal/plugins/lookup/test_lookupview.py
@@ -60,6 +60,9 @@ class _FakeTopLevelModel:
 
     def create_menu_items(self, *args):
         item = Gtk.MenuItem(label='Synonyms')
+        submenu = Gtk.Menu()
+        submenu.append(Gtk.MenuItem(label='sukmana'))
+        item.set_submenu(submenu)
         return [item]
 
 
@@ -87,6 +90,20 @@ def _make_view(plugins):
     view.lang_controller = _FakeLangController()
     view._pending_click_iter = None
     return view
+
+
+def test_top_level_model_items_with_a_submenu_show_their_children():
+    # A plain show() left a top-level item's own submenu (e.g.
+    # Thesaurus's actual synonym entries) built but never shown - an
+    # empty-looking flyout, even though the data was really there.
+    view = _make_view({'thesaurus': _FakeTopLevelModel()})
+    menu = Gtk.Menu()
+
+    view._on_populate_popup(_FakeTextbox('word'), menu)
+
+    synonyms_item = next(i for i in menu.get_children() if i.get_label() == 'Synonyms')
+    assert synonyms_item.get_submenu() is not None
+    assert all(child.get_visible() for child in synonyms_item.get_submenu().get_children())
 
 
 def test_top_level_model_items_go_directly_into_the_context_menu():
