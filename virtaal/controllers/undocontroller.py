c57319b24fd475fbbe172aca6527737d01b4d29c
Dwayne Bailey <dwayne@translate.org.za>
2026-09-17 11:47:39 +0100

    Update undo/redo sensitivity on real typing too
    _on_unit_insert_text/_on_unit_delete_text push directly onto the undo
    stack - the previous fix only updated sensitivity from
    push_current_text(), a separate, rarely-used path. Real typing left
    Undo stuck at its initial disabled state, blocking its own Ctrl+Z
    accelerator.

--- virtaal/controllers/undocontroller.py
+++ virtaal/controllers/undocontroller.py
@@ -286,6 +286,7 @@ class UndoController(BaseController):
         if pan_app.DEBUG:
             data['desc'] = 'offset=%d, deleted="%s", parent=%s, cursor_pos=%d, elem=%s' % (offset, repr(deleted), repr(parent), cursor_pos, repr(elem))
         self.model.push(data)
+        self._update_sensitivity()
 
     @if_enabled
     def _on_unit_insert_text(self, unit_controller, unit, ins_text, offset, elem, target_num):
@@ -311,3 +312,4 @@ class UndoController(BaseController):
         if pan_app.DEBUG:
             data['desc'] = 'ins_text="%s", offset=%d, elem=%s' % (ins_text, offset, repr(elem))
         self.model.push(data)
+        self._update_sensitivity()
